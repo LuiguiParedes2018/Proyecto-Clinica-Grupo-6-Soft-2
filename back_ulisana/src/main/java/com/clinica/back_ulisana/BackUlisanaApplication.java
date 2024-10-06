4105afdd1a97454ad570d99b807cce42d856662b
20184257@aloe.ulima.edu.pe <20184257@aloe.ulima.edu.pe>
2024-10-05 19:36:49 -0500

    backend base

---
+++ back_ulisana/src/main/java/com/clinica/back_ulisana/BackUlisanaApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.back_ulisana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackUlisanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackUlisanaApplication.class, args);
+	}
+
+}
---
+++ back_ulisana/src/test/java/com/clinica/back_ulisana/BackUlisanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.back_ulisana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackUlisanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
